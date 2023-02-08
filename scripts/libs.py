ea4d99c153380cfdcf82bf6fe3fab8253e48518d
Null-Guy <86558426+NullGuy-dev@users.noreply.github.com>
2023-02-08 15:19:25 +0200

    Add files via upload

---
+++ scripts/libs.py
@@ -0,0 +1,10 @@
+from PIL import Image
+from mss import mss
+from gtts import gTTS
+from time import sleep
+from threading import *
+from sqlalchemy import null
+from translate import Translator
+import mediapipe as mp
+import json, pyaudio, pyautogui, sys, playsound, cv2, os, random, webbrowser, datetime, pyspeedtest, pyperclip, geocoder, stun, calendar
+from vosk import Model, KaldiRecognizer
