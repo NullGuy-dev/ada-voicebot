f80fe0eeb89608b9a1cf88522761ff7772b3e80d
Roman Ershov <48359688+Roman4iks@users.noreply.github.com>
2023-02-28 13:54:53 +0200

    Create settings.py
    Необходимые HEADER данные для работы с сайтом.

---
+++ moduls/SwapDesktopPicture/settings.py
@@ -0,0 +1,23 @@
+HEADERS = {
+    'authority': 'ru.wallpaper.mob.org',
+    'accept': '*/*',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    # 'cookie': '_gat_gtag_UA_3685337_16=1; new_design=0; up-to-date=1; _ga=GA1.2.185645672.1677408071; _gid=GA1.2.843073875.1677408071; _gat_gtag_UA_3685337_16=1',
+    'origin': 'https://ru.wallpaper.mob.org',
+    'referer': 'https://ru.wallpaper.mob.org/',
+    'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest',
+}
+
+DATA = {
+    'pc': '',
+    'skip': '0',
+    'limit': '8',
+}
